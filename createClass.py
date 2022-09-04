f2b065c6750d5e704ab6e03ff478ad9de41f6c6f
Supratim-Pathak <supratimpathak@gmail.com>
2022-09-04 20:23:12 +0530

    xfb

---
+++ createClass.py
@@ -0,0 +1,19 @@
+class Mobile:
+    def __init__(self):
+        self.model = "Xiomi note 4"
+    def getModel(self):
+        print(self.model)
+
+class MobileNoTo:
+    def __init__(self, m):
+        self.model = m
+    def getModel(self):
+        print(self.model)
+        
+# CREATING AN OBJECT
+
+obj = Mobile()
+obj.getModel()
+
+obj2 = MobileNoTo('Samsung')
+obj2.getModel()
